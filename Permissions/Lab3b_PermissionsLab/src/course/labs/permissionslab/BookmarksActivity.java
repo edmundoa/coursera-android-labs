c0e5f42c5596f65569bb7dc4d3d73139c2c8235f
Edmundo Alvarez <edmundo@edmundoa.com>
2015-09-21 23:28:47 +0200

    Complete permissions lab

--- Permissions/Lab3b_PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
+++ Permissions/Lab3b_PermissionsLab/src/course/labs/permissionslab/BookmarksActivity.java
@@ -78,8 +78,8 @@ public class BookmarksActivity extends Activity {
 
 		Log.i(TAG, "Entered startGoToDangerousActivity()");
 
-		// TODO - Start the GoToDangerousActivity
-		
+		final Intent intent = new Intent(this, GoToDangerousActivity.class);
+		startActivity(intent);
 
 	}
 
